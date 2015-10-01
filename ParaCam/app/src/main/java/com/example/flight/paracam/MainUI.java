31c41f75ecb17bf9eef2b3bbb9ad92fb61222396
bcarth <bcarthel169@tamu.edu>
2015-10-01 10:50:31 -0500

    Intro menu and logo update
    Adding a main UI window and updated logos

--- ParaCam/app/src/main/java/com/example/flight/paracam/MainUI.java
+++ ParaCam/app/src/main/java/com/example/flight/paracam/MainUI.java
@@ -1,24 +1,22 @@
 package com.example.flight.paracam;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainUI extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_ui);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main_ui, menu);
         return true;
     }
 
@@ -35,10 +33,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void beginUI(View view) {
-        Intent intent = new Intent(this, MainUI.class);
-        startActivity(intent);
     }
 }
