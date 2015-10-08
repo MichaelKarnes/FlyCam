9fcbcb207015a36360135a45f0c819a083021649
MichaelKarnes <mkarnes2015@gmail.com>
2015-10-08 13:30:07 -0500

    Test Commit

--- ParaCam/app/src/androidTest/java/com/example/flight/paracam/ApplicationTest.java
+++ ParaCam/app/src/androidTest/java/com/example/flight/paracam/ApplicationTest.java
@@ -5,7 +5,7 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+ *//*Test*/
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
